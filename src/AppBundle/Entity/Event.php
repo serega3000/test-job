a0d886a1f3449c1e2e97ffc47c8ead017eb6bfe1
Sergey Sachik <serega3000@gmail.com>
2015-06-18 04:59:43 +0300

    Event class updated

--- src/AppBundle/Entity/Event.php
+++ src/AppBundle/Entity/Event.php
@@ -7,7 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass="AppBundle\Repository\PostRepository")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
  *
  * Event class
  */
